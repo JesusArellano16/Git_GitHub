ec721a94532e37eb5ce0c26723a15796b9727c04
Jesus Arellano <jesusarellano.ja50@gmail.com>
2022-04-06 23:56:50 -0500

    Se cambió el titulo del appBar (Curso Git y GitHub)

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
-      home: const MyHomePage(title: 'Rama appBar'),
+      home: const MyHomePage(title: 'Curso Git y GitHub'),
     );
   }
 }
