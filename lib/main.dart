e6430b4674f354d2053df4cc5141b46115fece6a
JesusArellano16 <79489697+JesusArellano16@users.noreply.github.com>
2022-04-07 14:00:47 -0500

    Comenzando con lo bueno de Git y GitHub

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
-      home: const MyHomePage(title: 'GitHub vs GitLab'),
+      home: const MyHomePage(title: 'Apenas empieza lo bueno'),
     );
   }
 }
